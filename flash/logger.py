1cf07269571805c199046628481e3ca45ce97435
Raghvender <raghvender1205@gmail.com>
2022-03-29 11:44:52 +0530

    Added a Valid Logger

---
+++ flash/logger.py
@@ -0,0 +1,7 @@
+import logging
+
+handler = logging.StreamHandler()
+handler.setFormatter(logging.Formatter("%(asctime)s %(levelname)s %(message)s"))
+logger = logging.getLogger("tez")
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
