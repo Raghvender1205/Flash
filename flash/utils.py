a03ccbf982dfcceb10f49c8dc884ffc1d75cfc15
Raghvender <raghvender1205@gmail.com>
2022-03-29 11:40:26 +0530

    AverageMeter, TPU and Seed Functions Added

---
+++ flash/utils.py
@@ -0,0 +1,50 @@
+import os
+import random
+
+import numpy as np
+import torch
+
+
+# Check for TPU
+def is_tpu_available():
+    try:
+        import torch_xla # PyTorch for XLA/TPU
+        return True
+    except ImportError:
+        return False
+
+
+class AverageMeter:
+    """
+    Computes and stores the average and the current value
+    """
+    def __init__(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+
+# Seed Everything and Set Backend for Computing
+def seed_everything(seed: int):
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = True
+    
