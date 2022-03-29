da5a3007b2c7f567a0b59b7916362e84ddb46556
Raghvender <raghvender1205@gmail.com>
2022-03-29 20:40:58 +0530

    TQDM based ProgressBar for Training

---
+++ flash/callbacks/progress.py
@@ -0,0 +1,47 @@
+from tqdm.auto import tqdm
+
+from flash.callbacks import Callback
+
+class Progress(Callback):
+    def __init__(self, num_train_steps, num_valid_steps):
+        self.num_train_steps = num_train_steps
+        self.num_valid_steps = num_valid_steps
+        self._train_tqdm = None
+
+    def on_train_start(self, flash_trainer, **kwargs):
+        self._train_tqdm = tqdm(total=self.num_train_steps)
+    
+
+    def format_metrics(self, metrics, stage):
+        epoch = metrics['epoch']
+        metrics_str = ', '.join(["{}: {:.4f}".format(k, v) for k, v in metrics.items() if k != 'epoch'])
+        return f"[{stage} e={epoch}] {metrics_str}"
+
+
+    def on_train_step_end(self, flash_trainer, **kwargs):
+        train_metrics = flash_trainer.metrics['train']
+        # print(train_metrics)
+        # self._train_tqdm.set_postfix(epoch=flash_trainer.current_epoch, **train_metrics)
+        self._train_tqdm.update(1)
+
+
+    def on_valid_epoch_end(self, flash_trainer, **kwargs):
+        # check if there are any validation metrics
+        if len(flash_trainer.metrics["valid"]) == 0:
+            return
+
+        train_metrics = flash_trainer.metrics["train"]
+        train_metrics["epoch"] = flash_trainer.current_epoch
+        # train_metrics["batches"] = flash_trainer.current_train_step
+        train_metrics = self.format_metrics(train_metrics, stage="train")
+        self._train_tqdm.write(train_metrics)
+
+        valid_metrics = flash_trainer.metrics["valid"]
+        valid_metrics["epoch"] = flash_trainer.current_epoch
+        # valid_metrics["batches"] = flash_trainer.current_train_step
+        valid_metrics = self.format_metrics(valid_metrics, stage="valid")
+        self._train_tqdm.write(valid_metrics)
+
+
+    def on_train_end(self, flash_trainer, **kwargs):
+        self._train_tqdm.close()
